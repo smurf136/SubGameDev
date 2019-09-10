b5ae64b678d9c9c50224cc30bb86396b3eed2f39
Smurf136 <apiwat.it24@mail.kmutt.ac.th>
2019-09-10 15:15:37 +0700

    [Fixed]
    GameScene

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,7 +1,12 @@
 let hp;
 let hpLog = 5;
+let cursors;
 let keyboards;
 let life = 5;
+let player;
+let Bullet;
+let bullet;
+let bullets;
 class GameScene extends Phaser.Scene {
     constructor(test) {
         super({
@@ -18,14 +23,9 @@ class GameScene extends Phaser.Scene {
 
     create() {
         hp = this.add.sprite(80, 50, 'hp').setScale(0.05) 
+        player = this.physics.add.sprite(287, 659, 'player')
 
-        this.player = new Player(
-            this,
-            this.game.config.width * 0.5,
-            this.game.config.height * 0.5,
-            'player'
-        );
-        
+
         this.anims.create({
             key: '5HP',
             frames: [ { key: 'hp', frame: 0 } ],
@@ -67,39 +67,76 @@ class GameScene extends Phaser.Scene {
             frames: [ { key: 'player', frame: 2 } ]
         })
 
-        
-        
-        this.player.anims.play('playerNotMove')
-        this.add.image(0, 0, "9");
-        this.Play = this.add.sprite(
-            this.game.config.width * 0.5,
-            this.game.config.height * 0.5,
-            "2"
-        );
-        
-       
-        this.Play.setInteractive();
+        cursors = this.input.keyboard.createCursorKeys()        
+        keyboards = this.input.keyboard.addKeys('W,A,S,D')
 
-        
-            
-        this.Play.on("pointerdown", function () {
-            this.Play.setTexture("2");
-           
-            this.scene.start('GameScene');
-        }, this);
+        var Bullet = new Phaser.Class({
 
-        
-        var text = this.add.text(250, 350, 'Play', { fill: '#00ff00' });
+            Extends: Phaser.GameObjects.Image,
     
+            initialize:
+    
+            function Bullet (scene)
+            {
+                Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
+    
+                this.speed = Phaser.Math.GetSpeed(700, 1);
+            },
+    
+            fire: function (x, y)
+            {
+                this.setPosition(x, y - 30);
+    
+                this.setActive(true);
+                this.setVisible(true);
+            },
+    
+            update: function (time, delta)
+            {
+                this.y -= this.speed * delta;
+    
+                if (this.y < -50)
+                {
+                    this.setActive(false);
+                    this.setVisible(false);
+                }
+            }
+    
+        });
 
+        
+        
+        player.anims.play('playerNotMove')
+        
+        bullets = this.physics.add.group({
+            classType: Bullet,
+            maxSize: 30,
+            runChildUpdate: true
+        });
+
+        console.log('bullets length: ' + bullets.getLength())
     }
     
     update() {
-        if(keyboards.up.isDown){
+        if(cursors.up.isDown){
             this.lossHealth()
-        }else if(keyboards.down.isDown){
+        }else if(cursors.down.isDown){
             this.healHealth()
         }
+
+        player.setVelocityY(0)
+        player.setVelocityX(0)
+
+        if(keyboards.W.isDown){
+            player.setVelocityY(-330)
+        }else if(keyboards.S.isDown){
+            player.setVelocityY(330)
+        }else if(keyboards.A.isDown){
+            player.setVelocityX(-330)
+        }else if(keyboards.D.isDown){
+            player.setVelocityX(330)
+        }
+
         if(hpLog >= 5){
             hp.anims.play('5HP')
         }else if(hpLog == 4){
@@ -114,6 +151,24 @@ class GameScene extends Phaser.Scene {
             hp.anims.play('0HP')
             this.isDie()
         }
+
+        console.log('player.x: ' + player.x)
+        console.log('player.y: ' + player.y)
+        
+        bullet = bullets.get();
+        if(bullet != null){
+            if (bullet){
+                bullet.setScale(0.05).setSize(0.05).fire(player.x, player.y);
+            }
+        }
+
+        bullets.children.each(function(b) {
+            if (b.active) {
+                if (b.y < 0) {
+                    b.setActive(false);
+                }
+            }
+        }.bind(this));
 
     }
 
@@ -133,5 +188,8 @@ class GameScene extends Phaser.Scene {
             hpLog = 5;
         }
     }
+
+    
 }
+
 export default GameScene;
